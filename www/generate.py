1716611b779987d094579e65fcbfee40cf7f73c9
Brendan Burns <brendan.d.burns@gmail.com>
2022-09-05 10:58:22 -0700

    Improve code generation script

--- www/generate.py
+++ www/generate.py
@@ -7,9 +7,12 @@ print("#define __DATA_H__")
 for file in sys.argv[1:]:
     basename = os.path.basename(file)
     sanitized = basename.replace('.', '_')
-    print("const char *{} = R\"{}(".format(sanitized, sanitized.upper()))
+    raw = sanitized
+    if len(raw) > 15:
+        raw = raw[0:16]
+    print("const char *{} = R\"{}(".format(sanitized, raw.upper()))
     with open(file, 'r') as f:
         print(f.read())
-    print("){}\";".format(sanitized.upper()))
+    print("){}\";".format(raw.upper()))
 
 print("#endif // __DATA_H__")
